efeb0b5185da7946e7f2204eb9c80146d8c06031
Alfred Gauthier <alfgoto@gmail.com>
2026-04-02 16:46:12 +0200

    fix: flagicon is back

--- front/src/app/apple-icon.tsx
+++ front/src/app/apple-icon.tsx
@@ -22,7 +22,7 @@ export default async function Icon() {
           borderRadius: "40px",
         }}
       >
-        <span style={{ fontSize: "100px" }}>💣</span>
+        <span style={{ fontSize: "100px" }}>🚩</span>
       </div>
     ),
     {
